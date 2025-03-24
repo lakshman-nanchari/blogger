42f79dd7da0701ce12fb817ff499e0e79939c30c
lakshman nanchari <lakshmannanchari96@gmail.com>
2025-03-24 15:06:50 +0530

    reqired changes

--- blog/serializers.py
+++ blog/serializers.py
@@ -11,7 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
     
 
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['passeord'])
+        validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
 
 class BlogSerializer(serializers.ModelSerializer):
